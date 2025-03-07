3b0fa02afcd5ec9a4a89f1cf4f3a8c3cf65bced4
dgarg7599 <gargdivyansh7599@gmail.com>
2025-03-07 13:31:27 +0530

    [Divyansh Garg] ADD: Implemented program to check if a string is a palindrome

---
+++ PalindromeCheck.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        String reversed = "";
+
+        // Reverse the string
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+
+        // Check if original string is equal to reversed string
+        if (str.equals(reversed)) {
+            System.out.println("The string is a palindrome.");
+        } else {
+            System.out.println("The string is not a palindrome.");
+        }
+    }
+}
