75106ad8dc4af6de821d3b01a9c7d2eea8659800
dgarg7599 <gargdivyansh7599@gmail.com>
2025-03-07 13:31:29 +0530

    [Divyansh Garg] ADD: Implemented program to toggle case of characters in a string

---
+++ ToggleCase.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ToggleCase {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (Character.isUpperCase(ch)) {
+                result.append(Character.toLowerCase(ch));
+            } else {
+                result.append(Character.toUpperCase(ch));
+            }
+        }
+        
+        System.out.println("Toggled case: " + result.toString());
+    }
+}
